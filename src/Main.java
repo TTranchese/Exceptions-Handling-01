8d3410138a08ab11bc57505d7d8bd2f57750bbc4
TTranchese <tommaso.tranchese.96@gmail.com>
2023-02-19 09:42:19 +0100

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+	public static void main(String[] args) {
+		checkScore(2.15);
+		checkScore(50);
+		checkScore(95.02);
+		checkScore(100.01);
+	}
+
+	public static void checkScore(double score) {
+		if (score>0&&score<=50.00) {
+			System.out.println("Average score");
+		} else if (score>50.00&&score<=100.00) {
+			System.out.println("Very good score");
+		} else {
+			throw new ArithmeticException("Score is out of scale!");
+		}
+	}
+}
